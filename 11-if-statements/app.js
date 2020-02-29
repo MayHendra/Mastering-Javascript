d74539326a6e0aee06d85b4986c766d4484e8050
MayHendra <mayhendra.p10@gmail.com>
2020-02-29 21:59:40 +0700

    belajar if else statment

---
+++ 11-if-statements/app.js
@@ -0,0 +1,23 @@
+/** @format */
+
+let number;
+
+// if (number == 100) {
+//   console.log('Yup');
+// } else {
+//   console.log('Nope');
+// }
+
+// if (number) {
+//   alert(`Number : ${number}`);
+// } else {
+//   alert('Number : NULL');
+// }
+
+number = 10;
+
+if (number > 50) {
+  alert('Yup');
+} else {
+  alert('Nope');
+}
