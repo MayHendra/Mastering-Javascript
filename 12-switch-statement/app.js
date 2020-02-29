6bd77bffa4fefc1002a4674a1485a2b9fd77efe2
MayHendra <mayhendra.p10@gmail.com>
2020-02-29 22:15:18 +0700

    Belajar Switch statement

---
+++ 12-switch-statement/app.js
@@ -0,0 +1,41 @@
+/** @format */
+
+// let color = prompt('Choose Your Colors');
+
+// switch (color) {
+//   case 'blue':
+//     alert('Color is Blue');
+//     break;
+//   case 'red':
+//     alert('Color is Red');
+//     break;
+//   case 'green':
+//     alert('Color is Green');
+//     break;
+//   default:
+//     alert('what colors ?');
+// }
+
+switch (new Date().getDay()) {
+  case 0:
+    alert("it's Sunday");
+    break;
+  case 1:
+    alert("it's Monday");
+    break;
+  case 2:
+    alert("it's Tuesday");
+    break;
+  case 3:
+    alert("it's Wednesday");
+    break;
+  case 4:
+    alert("it's Thursday");
+    break;
+  case 5:
+    alert("it's Friday");
+    break;
+  case 6:
+    alert("it's Saturday");
+    break;
+}
