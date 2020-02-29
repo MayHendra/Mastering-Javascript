9fbd327b96bdbb6c651340403cf8099c77d69a6d
MayHendra <mayhendra.p10@gmail.com>
2020-02-29 22:46:03 +0700

    belajar loop dengan for, while, dan, do while

---
+++ 14-general-loop/app.js
@@ -0,0 +1,25 @@
+/** @format */
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     console.log(`${i} is my favorit Number`);
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   if (i === 5) {
+//     console.log(`${i} is my favorit Number`);
+//   }
+//   i++;
+// }
+
+let i = 0;
+i = 100;
+do {
+  console.log(`Number : ${i}`);
+  i++;
+} while (i < 10);
